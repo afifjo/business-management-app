a9d4fd53d529d2453e73caeed986ef6db8e337bd
afifjo <afifjouili@hotmail.com>
2025-08-20 07:25:26 +0100

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755671125689

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_a7ab1ad6._.js",
+  "static/chunks/src_0fdc7ed8._.js"
+],
+    source: "dynamic"
+});
