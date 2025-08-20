aa5e6b009cbe5e912b86c003649bec5e83b7787a
afifjo <afifjouili@hotmail.com>
2025-08-20 07:25:00 +0100

    Add .next/static/chunks/src_app_dashboard_page_tsx_ee2405bb._.js 1755671100011

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_ee2405bb._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_11b89b41._.js"
+],
+    source: "dynamic"
+});
